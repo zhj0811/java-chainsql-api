b20e4ce7b9a1b8c8f743dc1630b8d2f45b8ac258
zyzhangyang <zhangyang@zhangyangdeMBP.lan>
2017-11-13 16:34:02 +0800

    编译错误修改

--- src/main/java/com/peersafe/base/client/Client.java
+++ src/main/java/com/peersafe/base/client/Client.java
@@ -689,7 +689,7 @@ public class Client extends Publisher<Client.events> implements TransportEventHa
             // Java-WebSocket transport implementation)
             //System.out.println("error_message: "+msg);
            // throw new RuntimeException(e);
-            e.printStackTrace;
+           e.printStackTrace();
         } finally {
             emit(OnStateChange.class, this);
         }
